538a22eecb96967567a3d2facbe7c30d388954a9
salih245 <salih_245@hotmail.com>
2026-05-20 13:45:29 -0400

    aa

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -30,7 +30,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
     
 @ensure_annotations
-def create_directories(path_to_directories: list, verbose: True):
+def create_directories(path_to_directories: list, verbose=True):
     """
     Creates a list of directories.
 
